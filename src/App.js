7249f56b86119610c674abbe6f84de41bfc4e96b
Jon Leopard <jontron2000@gmail.com>
2018-03-28 10:50:53 +0200

    Major refactor. Removed input fields since moonphase is the same despite location...duh. App is almost finished.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
-import { Provider, Flex, Box } from 'rebass';
+import { Provider, Flex, Box, Button } from 'rebass';
 import Masthead from './Masthead';
-import MoonPhaseForm from './Form/MoonPhaseForm';
 import Results from './MoonData/Results';
 import Footer from './Footer';
 import MoonBackground from './MoonBackground';
 
 const API_KEY = '2d50e8d7d184fe13';
 
-class App extends Component {
+class App extends React.Component {
   state = {
     country: undefined,
     city: undefined,
@@ -19,16 +18,36 @@ class App extends Component {
     isLoaded: false,
   };
 
-  getMoonPhase = async e => {
+  getNorthernMoonPhase = async e => {
     e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
     const api_call = await fetch(
-      `https://api.wunderground.com/api/${API_KEY}/astronomy/q/${country}/${city}.json`
+      `https://api.wunderground.com/api/${API_KEY}/astronomy/q/France/Paris.json`
     );
     const data = await api_call.json();
     console.log(data);
-    if (country && city) {
+    if (data) {
+      this.setState({
+        ageOfMoon: data.moon_phase.ageOfMoon,
+        phaseofMoon: data.moon_phase.phaseofMoon,
+        percentIlluminated: data.moon_phase.percentIlluminated,
+        error: '',
+      });
+    } else {
+      this.setState({
+        isLoaded: true,
+        error: 'Please enter the value',
+      });
+    }
+  };
+
+  getSouthernMoonPhase = async e => {
+    e.preventDefault();
+    const api_call = await fetch(
+      `https://api.wunderground.com/api/${API_KEY}/astronomy/q/SouthAfrica/CapeTown.json`
+    );
+    const data = await api_call.json();
+    console.log(data);
+    if (data) {
       this.setState({
         ageOfMoon: data.moon_phase.ageOfMoon,
         phaseofMoon: data.moon_phase.phaseofMoon,
@@ -52,7 +71,8 @@ class App extends Component {
           </Box>
           <Box w={1 / 2} pl={50}>
             <Masthead />
-            <MoonPhaseForm getMoonPhase={this.getMoonPhase} />
+            <Button onClick={this.getNorthernMoonPhase} children="North" />
+            <Button onClick={this.getSouthernMoonPhase} children="South" />
             <Results
               ageOfMoon={this.state.ageOfMoon}
               phaseofMoon={this.state.phaseofMoon}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { ApolloProvider } from 'react-apollo';
-import { ApolloClient } from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
-import { InMemoryCache } from 'apollo-cache-inmemory';
 
 // eslint-disable-next-line
 import globalStyles from './globalStyles';
 
-const httpLink = new HttpLink({ uri: 'http://localhost:4000' });
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
-
-ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
