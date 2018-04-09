55bb555e5491bbd72d2890ddb154d084ec15c961
Jon Leopard <jontron2000@gmail.com>
2018-04-09 11:51:32 +0200

    Moon data component added

---
+++ src/MoonData/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Flex, Box, Text } from 'rebass';
+
+const MoonData = props => (
+  <Flex>
+    <Box w={1 / 2} pt={3}>
+      {props.ageOfMoon && (
+        <Text
+          textAlign="left"
+          fontWeight="medium"
+          children={`Age of Moon: ${props.ageOfMoon}`}
+        />
+      )}
+      {props.phaseofMoon && (
+        <Text
+          textAlign="left"
+          fontWeight="medium"
+          children={`Current phase: ${props.phaseofMoon}`}
+        />
+      )}
+      {props.percentIlluminated && (
+        <Text
+          textAlign="left"
+          fontWeight="medium"
+          children={`Percent Illuminated: ${props.percentIlluminated} %`}
+        />
+      )}
+      {props.error && (
+        <Text
+          textAlign="left"
+          fontWeight="medium"
+          children={`Please enter the values ${props.error}`}
+        />
+      )}
+    </Box>
+  </Flex>
+);
+
+export default MoonData;
